5f9960b5c140728a8af16586a4009f20416772a8
Taim-Amer <taim.amer.000@gmail.com>
2024-12-20 05:45:25 -0800

    Auth Controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from "express"
 
+import { signup, login, logout } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("Signup Route")
-})
-
-router.get("/login", (req, res) => {
-    res.send("Login Route")
-})
-
-router.get("/logout", (req, res) => {
-    res.send("Logout Route")
-})
+router.post("/signup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router;
